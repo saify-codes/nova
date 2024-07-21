88d87706b4dd3ad49260bd2c2d2645aa3a8e9894
saify-codes <dev.muhammadsarfaraz@gmail.com>
2024-07-22 02:56:49 +0500

    fix users page ui

--- src/views/users.vue
+++ src/views/users.vue
@@ -3,9 +3,9 @@
 
     <Breadcrumb path="users"/>
 
-    <div class="relative overflow-x-auto p-1">
+    <div class="relative p-1">
       <div
-        class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900"
+        class="flex items-center justify-between gap-2 mb-4 bg-white dark:bg-gray-900"
       >
         <div>
           <button
@@ -72,8 +72,7 @@
             </div>
           </div>
         </div>
-        <label for="table-search" class="sr-only">Search</label>
-        <div class="relative">
+        <div class="relative flex-grow">
           <div
             class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none"
           >
@@ -97,86 +96,88 @@
             v-model="search"
             type="text"
             id="table-search-users"
-            class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="block p-2 ps-10 text-sm text-gray-900 w-full border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             placeholder="Search for users"
           />
         </div>
       </div>
-      <table
-        class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-      >
-        <thead
-          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
-        >
-          <tr>
-            <th scope="col" class="p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-all-search"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label for="checkbox-all-search" class="sr-only"
-                  >checkbox</label
-                >
-              </div>
-            </th>
-            <th scope="col" class="px-6 py-3">Name</th>
-            <th scope="col" class="px-6 py-3">Role</th>
-            <th scope="col" class="px-6 py-3">Status</th>
-            <th scope="col" class="px-6 py-3">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="user in users"
-            :key="user.id"
-            :data-id="user.id"
-            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+      <div class="overflow-x-auto">
+          <table
+            class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
           >
-            <td class="w-4 p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-table-search-1"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label for="checkbox-table-search-1" class="sr-only"
-                  >checkbox</label
-                >
-              </div>
-            </td>
-            <th
-              scope="row"
-              class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+            <thead
+              class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
             >
-              <Avatar :src="user.avatar" />
-              <div class="ps-3">
-                <div class="text-base font-semibold">{{ user.name }}</div>
-                <div class="font-normal text-gray-500">{{ user.email }}</div>
-              </div>
-            </th>
-            <td class="px-6 py-4">{{ user.role }}</td>
-            <td class="px-6 py-4">
-              <div class="flex items-center">
-                <div class="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div>
-                Online
-              </div>
-            </td>
-            <td class="px-6 py-4 space-x-3">
-              <button @click="deleteUser(user.id)">
-                <i class="fas fa-trash text-red-600"></i>
-              </button>
-              <button>
-                <i class="fas fa-pen text-green-600"></i>
-              </button>
-            </td>
-          </tr>
-          <tr v-show="!loading && users.length === 0">
-            <td class="px-6 py-4 text-center" colspan="5">No record...</td>
-          </tr>
-        </tbody>
-      </table>
+              <tr>
+                <th scope="col" class="p-4">
+                  <div class="flex items-center">
+                    <input
+                      id="checkbox-all-search"
+                      type="checkbox"
+                      class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 rounded focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label for="checkbox-all-search" class="sr-only"
+                      >checkbox</label
+                    >
+                  </div>
+                </th>
+                <th scope="col" class="px-6 py-3">Name</th>
+                <th scope="col" class="px-6 py-3">Role</th>
+                <th scope="col" class="px-6 py-3">Status</th>
+                <th scope="col" class="px-6 py-3">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="user in users"
+                :key="user.id"
+                :data-id="user.id"
+                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <td class="w-4 p-4">
+                  <div class="flex items-center">
+                    <input
+                      id="checkbox-table-search-1"
+                      type="checkbox"
+                      class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 rounded focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label for="checkbox-table-search-1" class="sr-only"
+                      >checkbox</label
+                    >
+                  </div>
+                </td>
+                <th
+                  scope="row"
+                  class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  <Avatar :src="user.avatar" />
+                  <div class="ps-3">
+                    <div class="text-base font-semibold">{{ user.name }}</div>
+                    <div class="font-normal text-gray-500">{{ user.email }}</div>
+                  </div>
+                </th>
+                <td class="px-6 py-4">{{ user.role }}</td>
+                <td class="px-6 py-4">
+                  <div class="flex items-center">
+                    <div class="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div>
+                    Online
+                  </div>
+                </td>
+                <td class="px-6 py-4 space-x-3">
+                  <button @click="deleteUser(user.id)">
+                    <i class="fas fa-trash text-red-600"></i>
+                  </button>
+                  <button>
+                    <i class="fas fa-pen text-green-600"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr v-show="!loading && users.length === 0">
+                <td class="px-6 py-4 text-center" colspan="5">No record...</td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
     </div>
 
     <!-- Loader -->
